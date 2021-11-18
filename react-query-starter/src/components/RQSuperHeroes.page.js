4ab9f339b9fd684d07e954d0b01dd6d35e490b73
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-18 01:05:19 -0500

    error state with react-query

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -6,17 +6,21 @@ const fetchSuperHeroes = () => {
 }
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes);
+  const {
+    isLoading,
+    data,
+    isError,
+    error
+  } = useQuery('super-heroes', fetchSuperHeroes);
 
-  return isLoading ?
-    (<h2>Loading</h2>)
-    :
-    (
-      <>
-        <h2>React Query Super Heroes Page</h2>
-        {data?.data.map((hero) => (
-          <div key={hero.name}>{hero.name}</div>
-        ))}
-      </>
-    )
+  return (
+    isLoading ? <h2>Loading</h2>
+      :
+      isError ? <h2>{error.message}</h2>
+        :
+        (<>
+          <h2>React Query Super Heroes Page</h2>
+          {data?.data.map(hero => <div key={hero.name}>{hero.name}</div>)}
+        </>)
+  )
 }
