7cc65d7194a2843cfebc134be8d543a550aa4fd0
HuseyinAtacanDemir <h.atacandemir@gmail.com>
2021-11-17 21:55:51 -0500

    react query sample fetch

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,3 +1,22 @@
+import { useQuery } from "react-query"
+import axios from "axios";
+
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
+  const { isLoading, data } = useQuery('super-heroes', async () => {
+    await new Promise(res => setTimeout(res, 1000));
+    return axios.get('http://localhost:4000/superheroes')
+  });
+
+  return isLoading ?
+    (<h2>Loading</h2>)
+    :
+    (
+      <>
+        <h2>React Query Super Heroes Page</h2>
+        {data?.data.map((hero) => (
+          <div key={hero.name}>{hero.name}</div>
+        ))}
+      </>
+    )
 }
